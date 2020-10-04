6718a12d4f14781914a04f08d3984ed7a8a9c725
jakears93 <jake.arsenault@hotmail.ca>
2020-10-04 14:57:41 -0400

    Completed quote parser, implemented addQuote function

--- index.js
+++ index.js
@@ -25,21 +25,28 @@ con.connect(function(err) {
 });
 
 bot.on('ready', () => {
-  	console.info(`Logged in as ${bot.user.tag}!`);
+  	console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 
 //Wait for message triggers
 bot.on('message', msg => {
-  	if (msg.content === '!quote') {
+     var parsedMsg = parseCommand(msg.content);
+     var command = parsedMsg[0];
+
+// TODO: Switch to switch statement
+  	if (command === '!quote') {
+          console.log("Current Command: "+command);
 		quote = generateRandomQuote();
     		msg.reply(quote);
   	}
-	else if (msg.content === '!help') {
+	else if (command === '!help') {
+          console.log("Current Command: "+command);
 		msg.channel.send(usage());
 	}
-     else if (msg.content === '!add') {
-          if (checkUser(msg))
+     else if (command === '!add') {
+          console.log("Current Command: "+command);
+          if(checkUser(msg))
           {
                addQuote(msg);
           }
@@ -48,67 +55,133 @@ bot.on('message', msg => {
 
 
 //Functions
+function parseCommand(msg)
+{
+     var command = msg.split(' ', 1);
+     return command;
+}
+
 function checkUser(msg)
 {
+     var returnFlag = true;
      var user = msg.member.user.id;
      var sql = "SELECT * FROM user WHERE user_id = "+msg.member.user.id+" limit 1;";
 
-     con.query(sql, function (err, result) {
+     con.query(sql, returnFlag = function (err, result) {
           if (err)
           {
-               console.log("Error in DB Checking User");
+               console.log("\tError in DB Checking User");
                return false;
           }
           else
           {
                if(result.length)
                {
-                    console.log(msg.member.user.tag+" already in db");
-                    sql = "UPDATE user SET no_of_quotes = no_of_quotes + 1 WHERE user_id LIKE '"+msg.member.user.id+"' ";
-                    con.query(sql, function (error, row){
-                         if (error)
-                         {
-                              console.log("Error adding quote to user");
-                              return false;
-                         }
-
-                    })
+                    console.log("\t"+msg.member.user.tag+" already in db");
                     return true;
                }
                else
                {
                     if(addUser(msg))
                     {
-                       return true;
+                         return true;
+                    }
+                    else
+                    {
+                         console.log("\tError Adding User to Db");
+                         returnFlag = 0;
+                         return false;
                     }
                }
           }
-          return false;
      });
+     return returnFlag;
 }
 
 function addUser(msg)
 {
+     var returnFlag = true;
      var id = msg.member.user.id;
      var user = msg.member.user.username;
      var sql = "INSERT INTO user (user_id, name) VALUES ('"+id+"','"+user+"')";
-     con.query(sql, function (err, result) {
+     con.query(sql, returnFlag = function (err, result) {
           if (err)
           {
-               console.log("Error in DB Adding User");
+               console.log("\tError in DB Adding User");
                return false;
           }
           else
           {
-               console.log(msg.member.user.tag+" Added to DB!")
+               console.log("\t"+msg.member.user.tag+" Added to DB!")
                return true;
           }
      });
+     return returnFlag;
 }
 
 function addQuote(msg, msgInfo)
 {
+     var returnFlag = false;
+     var parsedQuote = msg.content.match("\"[^\"]*\"{0,1}");     //parses anything between first two quotation marks
+     var parsedAuthor = msg.content.match("(?<=-)[^-].*");        //parses anything after a dash, excludes any new lines
+     var id = msg.member.user.id;
+     var channel = msg.channel.name;
 
+     if(parsedQuote === null || parsedAuthor === null)
+     {
+          console.log("\tImproper format, quote could not be added.");
+          msg.reply("Sorry! I am unable to add your quote.  Please make sure to use the !add \"Quote\" -Author format!");
+          return false;
+     }
+
+     var sql = "SELECT * FROM quote WHERE quote = "+parsedQuote+" limit 1;";
+
+     con.query(sql, function (err, result) {
+          if (err)
+          {
+               console.log("\tError in DB Checking Quote");
+               returnFlag = true;
+          }
+          else
+          {
+               if(result.length)
+               {
+                    msg.reply("Sorry! Your submitted quote is already in the database.");
+                    console.log("\tQuote is already in the db");
+                    returnFlag = true;
+               }
+          }
+     });
+
+     console.log("return flag: "+returnFlag);
+     if(returnFlag)
+          return false;
+
+     sql = "INSERT INTO quote (quote, user_id, channel, author) VALUES ("+parsedQuote+","+id+", '"+channel+"', '"+parsedAuthor+"')";
+     con.query(sql, function (err, result) {
+          if (err)
+          {
+               console.log(err);
+               console.log("\tError in Db Adding Quote");
+               returnFlag = true;
+          }
+          else
+          {
+               console.log("\t"+msg.member.user.tag+" Added a quote to the DB!")
+          }
+     });
+
+     if(returnFlag)
+          return false;
+
+     sql = "UPDATE user SET no_of_quotes = no_of_quotes + 1 WHERE user_id LIKE '"+id+"' ";
+     con.query(sql, function (error, row){
+          if (error)
+          {
+               console.log("\tError Adding quote to user");
+          }
+     });
+     return true;
 }
 
 function usage()
